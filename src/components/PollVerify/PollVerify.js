21e4095d24d6574c78d40e93154092c60be127d5
Kevin Cui <Kevincui113@gmail.com>
2022-04-24 11:21:33 -0400

    fixed poll page css

--- src/components/PollVerify/PollVerify.js
+++ src/components/PollVerify/PollVerify.js
@@ -16,12 +16,12 @@ const PollVerify = () => {
 
     return (
         <div className="options">
-            <h1>Select the right option</h1>
-            <Option key={1} id = {1} text={arr.options[0]} onClick={onClick} />
-            <Option key={2} id = {2} text={arr.options[1]} onClick={onClick} />
-            <Option key={3} id = {3} text={arr.options[2]} onClick={onClick} />
-            <Option key={4} id = {4} text={arr.options[3]} onClick={onClick} />
-            
+            <h1 className="title">Select the correct option</h1>
+            <Option key={1} id={1} text={arr.options[0]} onClick={onClick} />
+            <Option key={2} id={2} text={arr.options[1]} onClick={onClick} />
+            <Option key={3} id={3} text={arr.options[2]} onClick={onClick} />
+            <Option key={4} id={4} text={arr.options[3]} onClick={onClick} />
+
         </div>
     );
 };
