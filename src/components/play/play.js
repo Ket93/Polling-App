16dfbf2f518aa93aa6b992fc272ee9a177906954
andy-liuu <aliu3160@gmail.com>
2022-04-24 11:09:30 -0400

    converted Waiting into class component

--- src/components/play/play.js
+++ src/components/play/play.js
@@ -1,30 +1,6 @@
 import React from "react";
 import "./play.css";
 
-/*
-const Play = () => {
-  return (
-    <>
-      <div className="buttonContainer">
-        <h3 className="question">INCOMING QUESTION</h3>
-        <a class="slide_from_bottom" href="/waiting">
-          Option 1{" "}
-        </a>
-        <a class="slide_from_bottom" href="/waiting">
-          Option 2{" "}
-        </a>
-        <a class="slide_from_bottom" href="/waiting">
-          Option 3{" "}
-        </a>
-        <a class="slide_from_bottom" href="/waiting">
-          Option 4{" "}
-        </a>
-      </div>
-    </>
-  );
-};
-*/
-
 class Play extends React.Component {
   constructor(props) {
     super(props);
--- src/components/waiting/waiting.js
+++ src/components/waiting/waiting.js
@@ -1,60 +1,62 @@
 import React from "react";
 import "./waiting.css";
 
-const Waiting = () => {
-  return (
-    <>
-      <head>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
-        </style>
-      </head>
-      <main>
-        <div className="dank-ass-loader">
-          <div className="row">
-            <div className="arrow up outer outer-18"></div>
-            <div className="arrow down outer outer-17"></div>
-            <div className="arrow up outer outer-16"></div>
-            <div className="arrow down outer outer-15"></div>
-            <div className="arrow up outer outer-14"></div>
+class Waiting extends React.Component {
+  render() {
+    return (
+      <>
+        <head>
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+          </style>
+        </head>
+        <main>
+          <div className="dank-ass-loader">
+            <div className="row">
+              <div className="arrow up outer outer-18"></div>
+              <div className="arrow down outer outer-17"></div>
+              <div className="arrow up outer outer-16"></div>
+              <div className="arrow down outer outer-15"></div>
+              <div className="arrow up outer outer-14"></div>
+            </div>
+            <div className="row">
+              <div className="arrow up outer outer-1"></div>
+              <div className="arrow down outer outer-2"></div>
+              <div className="arrow up inner inner-6"></div>
+              <div className="arrow down inner inner-5"></div>
+              <div className="arrow up inner inner-4"></div>
+              <div className="arrow down outer outer-13"></div>
+              <div className="arrow up outer outer-12"></div>
+            </div>
+            <div className="row">
+              <div className="arrow down outer outer-3"></div>
+              <div className="arrow up outer outer-4"></div>
+              <div className="arrow down inner inner-1"></div>
+              <div className="arrow up inner inner-2"></div>
+              <div className="arrow down inner inner-3"></div>
+              <div className="arrow up outer outer-11"></div>
+              <div className="arrow down outer outer-10"></div>
+            </div>
+            <div className="row">
+              <div className="arrow down outer outer-5"></div>
+              <div className="arrow up outer outer-6"></div>
+              <div className="arrow down outer outer-7"></div>
+              <div className="arrow up outer outer-8"></div>
+              <div className="arrow down outer outer-9"></div>
+            </div>
           </div>
-          <div className="row">
-            <div className="arrow up outer outer-1"></div>
-            <div className="arrow down outer outer-2"></div>
-            <div className="arrow up inner inner-6"></div>
-            <div className="arrow down inner inner-5"></div>
-            <div className="arrow up inner inner-4"></div>
-            <div className="arrow down outer outer-13"></div>
-            <div className="arrow up outer outer-12"></div>
-          </div>
-          <div className="row">
-            <div className="arrow down outer outer-3"></div>
-            <div className="arrow up outer outer-4"></div>
-            <div className="arrow down inner inner-1"></div>
-            <div className="arrow up inner inner-2"></div>
-            <div className="arrow down inner inner-3"></div>
-            <div className="arrow up outer outer-11"></div>
-            <div className="arrow down outer outer-10"></div>
-          </div>
-          <div className="row">
-            <div className="arrow down outer outer-5"></div>
-            <div className="arrow up outer outer-6"></div>
-            <div className="arrow down outer outer-7"></div>
-            <div className="arrow up outer outer-8"></div>
-            <div className="arrow down outer outer-9"></div>
-          </div>
-        </div>
 
-        <div>
-          <h2 className="message1">Your response has been submitted.</h2>
-          <h2 className="message2">
-            Keep watching to see if you were correct!
-          </h2>
-        </div>
-      </main>
-    </>
-  );
-};
+          <div>
+            <h2 className="message1">Your response has been submitted.</h2>
+            <h2 className="message2">
+              Keep watching to see if you were correct!
+            </h2>
+          </div>
+        </main>
+      </>
+    );
+  }
+}
 
 export default Waiting;
