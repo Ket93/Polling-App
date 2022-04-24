e658c4c9162ffceb34a5974b91bd139a17e00de1
andy-liuu <aliu3160@gmail.com>
2022-04-24 09:56:52 -0400

    Play component calls api

--- src/components/play/play.js
+++ src/components/play/play.js
@@ -1,20 +1,68 @@
 import React from "react";
 import "./play.css";
 
+/*
 const Play = () => {
+  return (
+    <>
+      <div className="buttonContainer">
+        <h3 className="question">INCOMING QUESTION</h3>
+        <a class="slide_from_bottom" href="/waiting">
+          Option 1{" "}
+        </a>
+        <a class="slide_from_bottom" href="/waiting">
+          Option 2{" "}
+        </a>
+        <a class="slide_from_bottom" href="/waiting">
+          Option 3{" "}
+        </a>
+        <a class="slide_from_bottom" href="/waiting">
+          Option 4{" "}
+        </a>
+      </div>
+    </>
+  );
+};
+*/
+
+class Play extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: {}, answers: [] };
+  }
+
+  async componentDidMount() {
+    const url = "http://localhost:3301/6264df6fd4646c90ecafa365";
+    //const url = "http://localhost:3301/" + this.props.id;
+    const response = await fetch(url);
+    const json = await response.json();
+    this.setState({ data: json["data"] });
+    this.setState({ answers: json["data"]["answers"] });
+  }
+
+  render() {
+    console.log(this.state.answers[1]);
 
     return (
-
-        <>
-            <div className="buttonContainer">
-                <h3 className="question">INCOMING QUESTION</h3>
-                <a class="slide_from_bottom" href="/waiting">Option 1 </a>
-                <a class="slide_from_bottom" href="/waiting">Option 2 </a>
-                <a class="slide_from_bottom" href="/waiting">Option 3 </a>
-                <a class="slide_from_bottom" href="/waiting">Option 4 </a>
-            </div>
-        </>
+      <>
+        <div className="buttonContainer">
+          <h3 className="question">{this.state.data["question"]}</h3>
+          <a class="slide_from_bottom" href="/waiting">
+            <span>{this.state.answers[0]}</span>
+          </a>
+          <a class="slide_from_bottom" href="/waiting">
+            <span>{this.state.answers[1]}</span>
+          </a>
+          <a class="slide_from_bottom" href="/waiting">
+            <span>{this.state.answers[2]}</span>
+          </a>
+          <a class="slide_from_bottom" href="/waiting">
+            <span>{this.state.answers[3]}</span>
+          </a>
+        </div>
+      </>
     );
-};
+  }
+}
 
 export default Play;
