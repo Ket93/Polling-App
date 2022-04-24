ca7e1445f7c2cf81c61720845cc7a11825af4511
Anthony Ho <anthony52ho@gmail.com>
2022-04-24 01:07:32 -0400

    made changes

--- src/components/wrong/wrong.js
+++ src/components/wrong/wrong.js
@@ -10,12 +10,13 @@ const Wrong = () => {
 
             <div class="stylediv">
                 <div class="white">
-                    <div class="checkmark-circle">
-                        <div class="background"></div>
-                        <div class="checkmark draw"></div>
+                    <div class="wrapper">
+                    <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                            <circle class="checkmark_circle" cx="26" cy="26" r="25" fill="none" />
+                            <path class="checkmark_check" fill="none" d="M14.1 14.1l23.8 23.8 m0,-23.8 l-23.8,23.8" /></svg>
                     </div>
-                    <h1>Congratulations!</h1>
-                    <h4>Your answer was correct! Well done!</h4>
+                    <h1>You guessed wrong :( </h1>
+                    <h4>Get good at the game, and contemplat</h4>
                     <h4>Stay tuned! The next question is coming soon!</h4>
 
                 </div>
