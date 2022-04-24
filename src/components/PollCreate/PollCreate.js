6c144d9887282b6fad474db75ee549a3640bc8a7
Anthony Ho <anthony52ho@gmail.com>
2022-04-24 00:28:50 -0400

    sad

--- src/components/PollCreate/PollCreate.js
+++ src/components/PollCreate/PollCreate.js
@@ -1,46 +1,57 @@
 import { Component, useState } from "react";
 import "./PollCreate.css";
-import { Button } from "./Button.js";
-import { ListComponent } from "./ListComponent.js";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from "react";
 
 const PollCreate = () => {
-    const [components, setComponents] = useState([0]);
+    const [o1, seto1] = useState("");
+    const [o2, seto2] = useState("");
+    const [o3, seto3] = useState("");
+    const [o4, seto4] = useState("");
 
-    function addComponent() {
-        setComponents([...components, components.length]);
-    }
+    const o1change = (e) => {
+        seto1(e.target.value);
+    };
+    const o2change = (e) => {
+        seto2(e.target.value);
+    };
+    const o3change = (e) => {
+        seto3(e.target.value);
+    };
+    const o4change = (e) => {
+        seto4(e.target.value);
+    };
 
-    function onDelete(id) {
-        console.log(id);
-        setComponents([
-            ...components.slice(0, id),
-            ...components.slice(id + 1),
-        ]);
-        console.log(components.length);
-    }
+    
 
-
-    function complete() {
-        var form = document.querySelectorAll("name");
-        console.log(form);
-    }
+    const Submit = (e) => {
+        console.log(o1, o2, o3, o4);
+    };
 
     return (
-        <div>
-            <Button onClick={addComponent} text="Call Component" />
-            {components.map((item, i) => (
-                <ListComponent
-                    key={i}
-                    id={i}
-                    text={item}
-                    onDelete={onDelete}
-                    complete={complete}
-                />
-            ))}
-            <Button onClick={complete} text="Submit" />
+        <div className='buttonContainer text-center' >
+            <form>
+                <h1>Option 1</h1>
+                <div className='row'>
+                    <label><input type="text" onChange={o1change}/></label>
+                </div>
+                <h1>Option 2</h1>
+                <div className='row'>
+                    <label><input type="text"  onChange={o2change} /></label>
+                </div>
+                <h1>Option 3</h1>
+                <div className='row'>
+                    <label><input type="text"  onChange={o3change} /></label>
+                </div>
+                <h1>Option 4</h1>
+                <div className='row'>
+                    <label><input type="text"  onChange={o4change} /></label>
+                </div>
+                <button type="button" onClick={Submit}>
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
